48b83c7a063eaf4e8526878b65ca89a63270d30c
Aleksa395 <167755824+Aleksa395@users.noreply.github.com>
2024-09-25 17:54:41 +0300

    Add files via upload

---
+++ Лабораторная работа 1.2/Лабораторная работа 1.2/Лабораторная работа 1.2.cpp
@@ -0,0 +1,22 @@
+﻿#include "stdio.h"
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main()
+{
+	srand(time(NULL));
+	int mass[10] = { 0 };
+	int maxx = 1000;
+	int minn = -1000;
+
+	for (int i = 0; i < 10; i++)
+	{
+		mass[i] = minn + rand() % (maxx - minn + 1);
+		printf("%d ", mass[i]);
+	}
+
+	getchar();
+}
+
